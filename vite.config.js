cb108e65c28276d43adc185107fa0b3c038c8f3a
DiamondOMG <omgdigital.developer.02@gmail.com>
2025-11-23 21:30:10 +0700

    remove server configuration from vite.config.js and add vercel.json for routing

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  server: {
-    // Optional: helps with SPA routing if using client-side router
-    historyApiFallback: true,
-  },
   build: {
     rollupOptions: {
       input: {
